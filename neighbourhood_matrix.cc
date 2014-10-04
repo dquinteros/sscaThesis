7019f53c4e84e6599297b10c65a5923c1b36365f
Daniel Quinteros <daniel.quinterosc@usach.cl>
2014-10-04 00:45:28 -0300

    Matrix clases

--- neighbourhood_matrix.cc
+++ neighbourhood_matrix.cc
@@ -1,48 +1,64 @@
 #include "neighbourhood_matrix.h"
 
-NeighbourhoodMatrix NeighbourhoodMatrix::NeighbourhoodMatrix(void)
+//Constructor vacio
+NeighbourhoodMatrix::NeighbourhoodMatrix(void)
 {
 	heigth = 0;
 	width = 0;		
 	neighbourhood_matrix = vector<vector<double>>(heigth, vector<double>(width));
 }
 
-NeighbourhoodMatrix NeighbourhoodMatrix::NeighbourhoodMatrix(vector<vector<double>> neighbourhood_matrix_)
+//Constructor solo con el vector
+NeighbourhoodMatrix::NeighbourhoodMatrix(vector<vector<double>> neighbourhood_matrix_)
 {
 	heigth = neighbourhood_matrix_.size();
 	width = neighbourhood_matrix_.front().size();
 	neighbourhood_matrix = vector<vector<double>>(neighbourhood_matrix_);
 }
 
-NeighbourhoodMatrix NeighbourhoodMatrix::NeighbourhoodMatrix(vector<vector<double>> neighbourhood_matrix_,int height_,int width_)
+//Constructor con vector y tamaño
+NeighbourhoodMatrix::NeighbourhoodMatrix(vector<vector<double>> neighbourhood_matrix_,int height_,int width_)
 {
 	height = height_;
 	width = width_;
 	neighbourhood_matrix = vector<vector<double>>(neighbourhood_matrix_);
 }
-
-void NeighbourhoodMatrix::ReadNeighbourhoodMatrix(char* filename){
+//Lectura
+NeighbourhoodMatrix NeighbourhoodMatrix::ReadNeighbourhoodMatrix(char* filename) const {
 	
+	NeighbourhoodMatrix read_matrix;
 	ifstream file(filename);
 
 	if(!file)
 	{
 		fprintf( stderr, "ERROR: the specified file could not be loaded\n");
-		return lines;
+		return read_matrix;
 	}
 
-	heigth = 0;
-	width = 0;		
-	neighbourhood_matrix = vector<vector<double>>(heigth, vector<double>(width));
 	
+	vector<double> matrix_line;
 	string file_line;	
 
 	while (getline(file, file_line))
-	{
-		vector<double> matrix_line;
-		if (line[0]=='#'|| line[0]=='\0') continue;        
-		boost::split(matrix_line,file_line,boost::is_any_of(" "));
-		neighbourhood_matrix.push_back(matrix_line);  
+	{		
+		if (file_line[0]=='#'|| file_line[0]=='\0') continue;
+		istringstream is(file_line);
+		double point_probability;
+		while(is >> point_probability)
+		{
+		   matrix_line.push_back(point_probability);
+		}
+		read_matrix.neighbourhood_matrix.push_back(matrix_line); 
+		matrix_line.clear(); 
 	}   
-	
+	read_matrix.width =  neighbourhood_matrix.front().size();
+	read_matrix.heigth = neighbourhood_matrix.size();
+
+	return read_matrix;
+}
+
+//Escritura
+void WriteNeigbourhoodMatrix(char* filename)
+{
+
 }
--- neighbourhood_matrix.h
+++ neighbourhood_matrix.h
@@ -2,11 +2,11 @@
 #ifndef __NEIGHBOURHOOD_MATRIX_H__
 #define __NEIGHBOURHOOD_MATRIX_H__
 
-#include <iostream.h>
-#include <fstream.h>
-#include <vector.h>
-
-#include <boost/algorithm/string/split.hpp>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -21,8 +21,12 @@ public:
 	NeighbourhoodMatrix(void);
 	NeighbourhoodMatrix(vector<vector<double>> neighbourhood_matrix);
 	NeighbourhoodMatrix(vector<vector<double>> neighbourhood_matrix,int height,int width);
+	//Getters
+	vector<vector<double>> GetNeighbourhoodMatrix(void){return neighbourhood_matrix;}
+	int GetHeight(void){return height;}
+	int GetWidth(void){return width;}
 	//Lectura & Escritura
-	void ReadNeighbourhoodMatrix(char* filename);
+	void ReadNeighbourhoodMatrix(char* filename) const;
 	void WriteNeigbourhoodMatrix(char* filename);
 	//Sobrecarga de operador asignacion
 	NeighbourhoodMatrix& operator= (const NeighbourhoodMatrix&);
