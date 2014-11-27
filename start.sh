bb3e570161672ecfa3dc8fa3a4b6256b320ad763
Daniel Quinteros <daniel.quinterosc@usach.cl>
2014-11-27 17:51:47 -0300

    list files & bash update

--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-make clean && make && ./main.o -p list_files/train_pos_list_files.lst -n list_files/train_neg_list_files.lst -c svm -t list_files/test_pos_list_files.lst
+rm master.zip
+wget https://github.com/dquinteros/sscaThesis/archive/master.zip
+unzip -u master.zip
+cd sscaThesis-master
+make clean && make
+screen -d -m ./main.o -p list_files/train_pos_list_files.lst -n list_files/train_neg_list_files.lst -c svm -t list_files/test_pos_list_files_1.lst
+screen -d -m ./main.o -p list_files/train_pos_list_files.lst -n list_files/train_neg_list_files.lst -c svm -t list_files/test_pos_list_files_2.lst
+screen -d -m ./main.o -p list_files/train_pos_list_files.lst -n list_files/train_neg_list_files.lst -c svm -t list_files/test_pos_list_files_3.lst
+screen -d -m ./main.o -p list_files/train_pos_list_files.lst -n list_files/train_neg_list_files.lst -c svm -t list_files/test_pos_list_files_4.lst
+screen -d -m ./main.o -p list_files/train_pos_list_files.lst -n list_files/train_neg_list_files.lst -c svm -t list_files/test_pos_list_files_5.lst
+screen -d -m ./main.o -p list_files/train_pos_list_files.lst -n list_files/train_neg_list_files.lst -c svm -t list_files/test_pos_list_files_6.lst
+screen -d -m ./main.o -p list_files/train_pos_list_files.lst -n list_files/train_neg_list_files.lst -c svm -t list_files/test_pos_list_files_7.lst
+screen -d -m ./main.o -p list_files/train_pos_list_files.lst -n list_files/train_neg_list_files.lst -c svm -t list_files/test_pos_list_files_8.lst
+
 #for i in pruebas/*.mat; do
 #	    python pysrc/statsValue.py $i
 #donein
